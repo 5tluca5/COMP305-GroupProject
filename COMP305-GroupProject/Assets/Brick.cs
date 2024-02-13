095ba1869f666bf51ad7a82be23de215880dde0a
John Celso Inocente <jinocen1@my.centennialcollege.ca>
2024-02-13 15:05:37 -0500

    Added Tile prefabs. Added GameOver screen.

--- COMP305-GroupProject/Assets/Brick.cs
+++ COMP305-GroupProject/Assets/Brick.cs
@@ -10,18 +10,23 @@ public class Brick : MonoBehaviour
     private SpriteRenderer _brickRenderer;
 
     [SerializeField]
-    private int _hp = 2;
+    private float _hp = 150.0f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("bullet"))
         {
-            _hp--;
-            if (_hp == 1)
+            Projectile bullet = collision.gameObject.GetComponent<Projectile>();
+             _hp -= bullet.GetDamage();
+            if (_hp >= 90)
             {
-                _brickRenderer.sprite = _brickImages[_hp];
+                _brickRenderer.sprite = _brickImages[1];
             }
-            if (_hp == 0)
+            if (_hp <= 50)
+            {
+                _brickRenderer.sprite = _brickImages[0];
+            }
+            if (_hp <= 0)
             {
                 Destroy(this.gameObject);
             }
--- COMP305-GroupProject/Assets/Scripts/Core/Projectile.cs
+++ COMP305-GroupProject/Assets/Scripts/Core/Projectile.cs
@@ -39,6 +39,11 @@ public class Projectile : MonoBehaviour
         body.AddForce(direction * this.speed);
     }
 
+    public float GetDamage() 
+    {
+        return damage;
+    }
+
     [ContextMenu("Do Something")]
     void AddForce()
     {
