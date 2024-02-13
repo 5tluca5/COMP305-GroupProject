869adc306b5e452309a7f65ba36d6439a4455a45
Lucas-PC <l34229776@gmail.com>
2024-02-13 15:30:24 -0500

    Remove brick collision check

--- COMP305-GroupProject/Assets/Brick.cs
+++ COMP305-GroupProject/Assets/Brick.cs
@@ -14,23 +14,23 @@ public class Brick : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("bullet"))
-        {
-            Projectile bullet = collision.gameObject.GetComponent<Projectile>();
-            _hp -= bullet.GetDamage();
-            if (_hp >= 90)
-            {
-                _brickRenderer.sprite = _brickImages[1];
-            }
-            if (_hp <= 50)
-            {
-                _brickRenderer.sprite = _brickImages[0];
-            }
-            if (_hp <= 0)
-            {
-                Destroy(this.gameObject);
-            }
-        }
+        //if (collision.gameObject.CompareTag("bullet"))
+        //{
+        //    Projectile bullet = collision.gameObject.GetComponent<Projectile>();
+        //    _hp -= bullet.GetDamage();
+        //    if (_hp >= 90)
+        //    {
+        //        _brickRenderer.sprite = _brickImages[1];
+        //    }
+        //    if (_hp <= 50)
+        //    {
+        //        _brickRenderer.sprite = _brickImages[0];
+        //    }
+        //    if (_hp <= 0)
+        //    {
+        //        Destroy(this.gameObject);
+        //    }
+        //}
     }
 
     public void BeingHit(ProjectileData data)
