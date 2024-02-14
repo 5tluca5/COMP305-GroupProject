46aa794492629201c59ba804efe565f5d8d93151
Lucas-PC <l34229776@gmail.com>
2024-02-13 20:20:24 -0500

    Revert "Merge branch 'main' of https://github.com/5tluca5/COMP305-GroupProject"
    This reverts commit dc77025c11a434928ec7db8ab162f34b04443b23, reversing
    changes made to d7703a6abff61b6060e222188a3d798d32b59b0b.

---
+++ COMP305-GroupProject/Assets/Brick.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] _brickImages;
+    [SerializeField]
+    private SpriteRenderer _brickRenderer;
+
+    [SerializeField]
+    private float _hp = 150.0f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("bullet"))
+        //{
+        //    Projectile bullet = collision.gameObject.GetComponent<Projectile>();
+        //    _hp -= bullet.GetDamage();
+        //    if (_hp >= 90)
+        //    {
+        //        _brickRenderer.sprite = _brickImages[1];
+        //    }
+        //    if (_hp <= 50)
+        //    {
+        //        _brickRenderer.sprite = _brickImages[0];
+        //    }
+        //    if (_hp <= 0)
+        //    {
+        //        Destroy(this.gameObject);
+        //    }
+        //}
+    }
+
+    public void BeingHit(ProjectileData data)
+    {
+        // do the logic here
+        _hp -= data.damage;
+        if (_hp >= 80 && _hp <= 150)
+        {
+            _brickRenderer.sprite = _brickImages[1];
+        }else if (_hp >= 1)
+        {
+            _brickRenderer.sprite = _brickImages[0];
+        }else 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ COMP305-GroupProject/Assets/Scripts/Core/PlayerTank.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : Tank
+{
+    [Header("Tank Part")]
+    protected Dictionary<TankParts, TankPart> tankParts = new Dictionary<TankParts, TankPart>();
+
+    protected override void SetupTank()
+    {
+        stat = GameManager.Instance.GetCurrentTankStat();
+        tankParts = GameManager.Instance.GetCurrentTankParts();
+
+        fireRate = 1 / (stat.fireRate * 0.1f);
+
+        trackLImg.sprite = trackRImg.sprite = AtlasLoader.Instance.GetSprite(tankParts[TankParts.Track].spriteName);
+        towerImg.sprite = AtlasLoader.Instance.GetSprite(tankParts[TankParts.Tower].spriteName);
+        hullImg.sprite = AtlasLoader.Instance.GetSprite(tankParts[TankParts.Hull].spriteName);
+        gunImg.sprite = AtlasLoader.Instance.GetSprite(tankParts[TankParts.Gun].spriteName);
+        gunConnectorImg.sprite = AtlasLoader.Instance.GetSprite(tankParts[TankParts.Gun].associateSpriteName);
+    }
+
+    protected override void BeingHit(ProjectileData data)
+    {
+        if (!data.isPlayer)
+        {
+            stat.health -= data.damage;
+        }
+
+        if (stat.health <= 0f)
+        {
+            // GG
+            Debug.Log("Gameover");
+            Time.timeScale = 0f;
+        }
+    }
+}
